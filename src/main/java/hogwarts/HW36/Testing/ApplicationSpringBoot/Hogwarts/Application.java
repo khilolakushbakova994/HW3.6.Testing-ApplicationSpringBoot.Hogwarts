7f6b8fab56d66868f1a4cdfda9a9ccdd14309ff5
khilola.kushbakova <xilola_kushbakova@mai.ru>
2024-01-24 10:03:27 +0100

    Initial commit

---
+++ src/main/java/hogwarts/HW36/Testing/ApplicationSpringBoot/Hogwarts/Application.java
@@ -0,0 +1,13 @@
+package hogwarts.HW36.Testing.ApplicationSpringBoot.Hogwarts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
